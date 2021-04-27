132d9bf7abe2980893613732f379cd66b7431843
Kleverson Bona <kleverson@evoplan.com.br>
2021-04-27 09:37:55 -0300

    final version

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+  <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask" />
+ 
+  <router-view :showAddTask="showAddTask"></router-view>
+  <Footer />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer,
+  },
+  data(){
+    return {
+      showAddTask:false,
+    }
+  },
+  methods:{
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask
+    },
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button v-show="inHomePage" @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'black' : 'green'" />
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+export default {
+    name:'Header',
+    components: {Button},
+    computed:{
+        inHomePage(){
+            return this.$route.path === "/"
+        }
+    },
+    props: {
+        title:{
+            type:String,
+            default:'Header title'
+        },
+        showAddTask:{
+            type:Boolean,
+            default:false
+        }
+    }
+}
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
